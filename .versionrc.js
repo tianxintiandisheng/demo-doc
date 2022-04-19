25e67286d68c3ee036e58217375f59e51c5a4eb6
“田迪生” <disheng.tds@digitalexpo.com>
2022-04-19 16:25:06 +0800

    chore(standard-version): 添加standard-version配置文件

---
+++ .versionrc.js
@@ -0,0 +1,25 @@
+module.exports = {
+  //types为Conventional Commits标准中定义，目前支持
+  //https://github.com/conventional-changelog/commitlint/tree/master/%40commitlint/config-conventional
+  types: [
+    { type: 'feat', section: '新特性' },
+    { type: 'fix', section: 'Bug修复' },
+    { type: 'docs', section: '文档' },
+    { type: 'chore', section: '配置项' },
+    { type: 'style', section: '格式' },
+    { type: 'refactor', section: '重构' },
+    { type: 'perf', section: '性能' },
+    { type: 'test', section: '测试' },
+    { type: 'build', section: '构建' },
+    { type: 'ci', section: 'CI', hidden: true },
+    { type: 'revert', section: '回滚', hidden: true },
+  ],
+  //需要server-version更新版本号的文件
+  bumpFiles: [
+    {
+      filename: 'package.json',
+      // The `json` updater assumes the version is available under a `version` key in the provided JSON document.
+      type: 'json',
+    },
+  ],
+};
