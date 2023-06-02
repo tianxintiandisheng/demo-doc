7feacf9d047cfb03c14a8a191588ec44dc3a8314
tds01003270 <tds01003270@alibaba-inc.com>
2023-06-02 16:59:49 +0800

    ci(root): 添加html-loader依赖,配置html文件的解析

--- .dumirc.ts
+++ .dumirc.ts
@@ -3,5 +3,15 @@ import { defineConfig } from 'dumi';
 export default defineConfig({
   themeConfig: {
     name: 'demo-doc',
+    footer: '<a href="https://beian.miit.gov.cn/" target="_blank" rel="noreferrer">浙ICP备2022009054号-1</a><br />Powered by <a href="https://d.umijs.org/" target="_blank" rel="noreferrer">dumi</a>',
+  },
+  chainWebpack(config) {
+    // 解析html文件
+    config.module
+      .rule('html-loader')
+      .test(/\.html$/)
+      .exclude.end()
+      .use('html-loader')
+      .loader('html-loader');
   },
 });
